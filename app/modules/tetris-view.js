2626b53592369242b49382208ab8e53e0052079c
csq <275091651@qq.com>
2016-11-14 22:24:44 +0800

    添加游戏渲染层代码

---
+++ app/modules/tetris-view.js
@@ -0,0 +1,12 @@
+export default class TetrisView{
+	// param = {
+	// 	hSize,
+	// 	vSize,
+	// 	cubeSize,//方块尺寸，以像素为单位（方块是正方形哦）
+	// 	x,//地图左上角横坐标，单位像素
+	// 	y//地图左上角纵坐标，单位像素
+	// }
+	constructor(param){
+
+	}
+}
