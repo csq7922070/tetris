fcbd28107d5c02620a6748844bc8b35925952557
csq <275091651@qq.com>
2016-12-31 15:19:32 +0800

    添加导航组件

---
+++ src/layouts/nav.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+
+class Nav extends Component{
+	render(){
+		return (
+			<nav>
+				<Link to="/">Home</Link>
+				<Link to="/detail/1">Detail</Link>
+			</nav>
+		);
+	}
+}
+
+export default Nav;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 
+import Frame from '../layouts/frame';
 import Home from '../views/home';
 import Detail from '../views/detail';
 
 const routes = (
 	<Router history={hashHistory}>
-		<Route path="/" component={Home} />
-		<Route path="/detail/:id" component={Detail} />
+		<Route path="/" component={Frame}>
+			<IndexRoute component={Home} />
+			<Route path="/detail/:id" component={Detail} />
+		</Route>
 	</Router>
 );
 
